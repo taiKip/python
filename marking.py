1b260494fa55cb3d63155d02a00c0c9803f1174b
tarus <victortarus6@gmail.com>
2025-09-16 10:22:27 +0930

    changes made

---
+++ marking.py
@@ -0,0 +1,16 @@
+# Assignment marking tool
+# tarvk002
+
+
+def get_user_input():
+    input("Report (0-30): ")
+    input("Program (0-70):")
+    input("Late submission (y/n)")
+
+
+
+fruits  = ['apple','banana', 'orange','kiwi','mango']
+
+
+for i in range(-2,0):
+    print(fruits[i])
